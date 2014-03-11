cc57c3354e99300e51879c96b9fb92a2f89dff65
12010994 <victor.petit@etudiant.univ-rennes1.fr>
2014-03-11 13:47:25 +0100

    Init : Premier Commit

---
+++ TP5_MRI/src/fr/istic/date/lb/RecevoirDate.java
@@ -0,0 +1,35 @@
+package fr.istic.date.lb;
+import com.rabbitmq.client.AMQP.Queue;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.QueueingConsumer;
+
+public class RecevoirDate {
+
+  private static final String EXCHANGE_NAME = "logs";
+
+  public static void main(String[] argv) throws Exception {
+
+    ConnectionFactory factory = new ConnectionFactory();
+    factory.setHost("localhost");
+    Connection connection = factory.newConnection();
+    Channel channel = connection.createChannel();
+
+    channel.exchangeDeclare(EXCHANGE_NAME, "fanout");
+    String queueName = "file_date";//channel.queueDeclare().getQueue();
+    channel.queueDeclare(queueName,false,false,false,null);//Bind(queueName, EXCHANGE_NAME, "");
+    
+    System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+    QueueingConsumer consumer = new QueueingConsumer(channel);
+    channel.basicConsume(queueName, true, consumer);
+
+    while (true) {
+      QueueingConsumer.Delivery delivery = consumer.nextDelivery();
+      String message = new String(delivery.getBody());
+
+      System.out.println(" [x] Received '" + message + "'");
+    }
+  }
+}
